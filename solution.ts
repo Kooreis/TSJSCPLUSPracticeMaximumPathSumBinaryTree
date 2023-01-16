5a6e79abea978306be9b1c1247ee41bcd00ddba1
Kooreis <kooreis.official@gmail.com>
2023-01-16 12:24:29 -0600

    This update introduces the BinaryTree class with a root property and a maxPathSum method. The maxPathSum method calculates the maximum path sum in the binary tree using a helper function 'calculate'. The helper function recursively calculates the maximum path sum that can be obtained starting from the current node and going down.

--- solution.ts
+++ solution.ts
@@ -1,5 +1,16 @@
-class Node {
-    left: Node | null = null;
-    right: Node | null = null;
-    constructor(public value: number) {}
+class BinaryTree {
+    root: Node | null = null;
+
+    maxPathSum(): number {
+        let maxSum = Number.NEGATIVE_INFINITY;
+        const calculate = (node: Node | null): number => {
+            if (node === null) return 0;
+            let left = Math.max(0, calculate(node.left));
+            let right = Math.max(0, calculate(node.right));
+            maxSum = Math.max(maxSum, left + right + node.value);
+            return Math.max(left, right) + node.value;
+        };
+        calculate(this.root);
+        return maxSum;
+    }
 }
