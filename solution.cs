b1b6b2c31e2fbf79e71e64b0156687d1efa8353b
Kooreis <kooreis.official@gmail.com>
2023-01-16 12:24:29 -0600

    This update introduces the findMaxSum function which is a helper function that recursively finds the maximum sum of the path that can be extended to input binary tree's root node.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,14 @@
-class Node {
-    constructor(val, left = null, right = null) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+let maxSum = Number.NEGATIVE_INFINITY;
+
+function findMaxSum(root) {
+    if (root === null) {
+        return 0;
     }
+
+    let left = Math.max(0, findMaxSum(root.left));
+    let right = Math.max(0, findMaxSum(root.right));
+
+    maxSum = Math.max(maxSum, left + right + root.val);
+
+    return Math.max(left, right) + root.val;
 }
